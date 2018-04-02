82fead0606f88ca223ed550fba6e1b7769b62d6e
st <st.email@qq.com>
2018-04-02 17:07:24 +0800

    init

---
+++ src/main/java/com/qiein/jupiter/JupiterApplication.java
@@ -0,0 +1,12 @@
+package com.qiein.jupiter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JupiterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JupiterApplication.class, args);
+    }
+}
