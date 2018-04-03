0dddc1b2ee108d06772de8a6c6f7c2f7df700220
st <st.email@qq.com>
2018-04-03 18:44:16 +0800

    修改 redis配置 新增 redis测试方法

---
+++ src/test/java/com/qiein/jupiter/RedisTest.java
@@ -0,0 +1,23 @@
+package com.qiein.jupiter;
+
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RedisTest {
+
+    @Autowired
+    private RedisTemplate<String ,String> redisTemplate;
+
+    @Test
+    public void testValueOperation(){
+        redisTemplate.opsForValue().set("hello","123" );
+    }
+}
