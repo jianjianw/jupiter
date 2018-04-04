8728e7addd29c171550a07cff6bf88b1b9905c99
st <st.email@qq.com>
2018-04-04 16:44:10 +0800

    增加BaseController

---
+++ src/main/java/com/qiein/jupiter/web/controller/BaseController.java
@@ -0,0 +1,7 @@
+package com.qiein.jupiter.web.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BaseController {
+}
--- src/main/java/com/qiein/jupiter/web/controller/StaffController.java
+++ src/main/java/com/qiein/jupiter/web/controller/StaffController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/staff")
-public class StaffController {
+public class StaffController extends BaseController {
 
     @Autowired
     private StaffService staffService;
