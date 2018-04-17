9e1db70c03d6d441765162d8fbfc291223d599d5
sheery <727095262@qq.com>
2018-04-17 10:16:14 +0800

    1

--- src/main/java/com/qiein/jupiter/web/service/impl/ChannelServiceImpl.java
+++ src/main/java/com/qiein/jupiter/web/service/impl/ChannelServiceImpl.java
@@ -1,9 +1,14 @@
 package com.qiein.jupiter.web.service.impl;
 
+import com.qiein.jupiter.web.entity.po.ChannelPo;
 import com.qiein.jupiter.web.service.ChannelService;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ChannelServiceImpl implements ChannelService {
 
+    @Override
+    public void createChannel(ChannelPo channelPo) {
+        
+    }
 }
