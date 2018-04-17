687f019fee6f64f2fd0267114c31553c87e46a76
st <st.email@qq.com>
2018-04-17 10:50:33 +0800

    增加 mybatis别名

--- src/main/java/com/qiein/jupiter/JupiterApplication.java
+++ src/main/java/com/qiein/jupiter/JupiterApplication.java
@@ -3,9 +3,11 @@ package com.qiein.jupiter;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
 @MapperScan("com.qiein.jupiter.web.dao")
+@EntityScan("com.qiein.jupiter.web.entity.*")
 public class JupiterApplication {
 
     public static void main(String[] args) {
