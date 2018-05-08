99e126e3a8c79b26e7148aa103758f4ee9f473f1
sheery <727095262@qq.com>
2018-05-08 16:30:38 +0800

    1

--- src/main/java/com/qiein/jupiter/web/controller/OuterController.java
+++ src/main/java/com/qiein/jupiter/web/controller/OuterController.java
@@ -5,14 +5,10 @@ import com.qiein.jupiter.constant.TipMsgConstant;
 import com.qiein.jupiter.util.MobileLocationUtil;
 import com.qiein.jupiter.util.ResultInfo;
 import com.qiein.jupiter.util.ResultInfoUtil;
-import com.qiein.jupiter.web.entity.po.StaffPO;
-import com.qiein.jupiter.web.entity.vo.RolePermissionVO;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/outer")
