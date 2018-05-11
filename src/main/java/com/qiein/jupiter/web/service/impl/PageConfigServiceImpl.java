644535fa4986696f84b5734dec41661afddcc024
JingChenglong <chengvs_long@126.com>
2018-05-11 11:59:43 +0800

    1

--- src/main/java/com/qiein/jupiter/web/service/impl/PageConfigServiceImpl.java
+++ src/main/java/com/qiein/jupiter/web/service/impl/PageConfigServiceImpl.java
@@ -1,18 +1,17 @@
 package com.qiein.jupiter.web.service.impl;
 
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 import com.qiein.jupiter.constant.DictionaryConstant;
-import com.qiein.jupiter.constant.NumberConstant;
 import com.qiein.jupiter.util.CollectionUtils;
 import com.qiein.jupiter.web.dao.PageConfigDao;
 import com.qiein.jupiter.web.entity.dto.PageFilterDTO;
 import com.qiein.jupiter.web.entity.po.PageConfig;
 import com.qiein.jupiter.web.service.PageConfigService;
-import org.apache.commons.collections4.ListUtils;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-import java.util.List;
-import java.util.Map;
 
 /**
  * 页面配置
@@ -20,62 +19,60 @@ import java.util.Map;
 @Service
 public class PageConfigServiceImpl implements PageConfigService {
 
-    @Autowired
-    private PageConfigDao pageConfigDao;
+	@Autowired
+	private PageConfigDao pageConfigDao;
 
+	/**
+	 * 根据供公司Id 和 角色获取配置
+	 *
+	 * @param companyId
+	 * @param role
+	 * @return
+	 */
+	@Override
+	public List<PageConfig> listPageConfigByCidAndRole(int companyId, String role) {
+		// TODO 有没有一种好的思路解决过滤问题
+		List<PageConfig> pageConfigs = pageConfigDao.listPageConfigByCidAndRole(companyId, role);
+		// 判断是否为空
+		if (CollectionUtils.isEmpty(pageConfigs)) {
+			// 取默认公司
+			pageConfigs = pageConfigDao.listPageConfigByCidAndRole(DictionaryConstant.COMMON_COMPANYID, role);
+		}
+		if (CollectionUtils.isNotEmpty(pageConfigs)) {
+			for (PageConfig pageConfig : pageConfigs) {
+				System.out.println(pageConfig);
+			}
+		}
+		return pageConfigs;
+	}
 
-    /**
-     * 根据供公司Id 和 角色获取配置
-     *
-     * @param companyId
-     * @param role
-     * @return
-     */
-    @Override
-    public List<PageConfig> listPageConfigByCidAndRole(int companyId, String role) {
-        //TODO 有没有一种好的思路解决过滤问题
-        List<PageConfig> pageConfigs = pageConfigDao.listPageConfigByCidAndRole(companyId, role);
-        //判断是否为空
-        if (CollectionUtils.isEmpty(pageConfigs)) {
-            //取默认公司
-            pageConfigs = pageConfigDao.listPageConfigByCidAndRole(DictionaryConstant.COMMON_COMPANYID, role);
-        }
-        if (CollectionUtils.isNotEmpty(pageConfigs)) {
-            for (PageConfig pageConfig : pageConfigs) {
+	@Override
+	public Map<String, List<PageFilterDTO>> getAllPageFilterMap(int companyId) {
+		// 状态
+		// 渠道
+		// 品牌
+		// 咨询方式
+		// 意向等级
+		// 婚期简述
+		// 咨询类型
+		// 预排时间简述
+		// 客服小组，根据不同的角色
+		// 推广
+		// 筛选
+		// 客服
+		// 门市
+		// 门店
+		// 预算范围
+		// 无效原因
+		// 流失原因
+		//
 
-            }
-        }
-        return pageConfigs;
-    }
+		return null;
+	}
 
-    @Override
-    public Map<String, List<PageFilterDTO>> getAllPageFilterMap(int companyId) {
-        //状态
-        //渠道
-        //品牌
-        //咨询方式
-        //意向等级
-        //婚期简述
-        //咨询类型
-        //预排时间简述
-        //客服小组，根据不同的角色
-        //推广
-        //筛选
-        //客服
-        //门市
-        //门店
-        //预算范围
-        //无效原因
-        //流失原因
-        //
-
-
-        return null;
-    }
-
-    private void getTitleFilter(PageConfig pageConfig) {
-//        if(){
-//
-//        }
-    }
+	// private void getTitleFilter(PageConfig pageConfig) {
+	// // if(){
+	// //
+	// // }
+	// }
 }
