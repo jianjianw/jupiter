49b59c33cdd4680c42efb6ee77f754a1a40edb00
JingChenglong <chengvs_long@126.com>
2018-05-11 11:58:18 +0800

    1

--- src/main/java/com/qiein/jupiter/msg/dingding/DingMsgUtil.java
+++ src/main/java/com/qiein/jupiter/msg/dingding/DingMsgUtil.java
@@ -1,24 +1,24 @@
 package com.qiein.jupiter.msg.dingding;
 
+import org.springframework.stereotype.Component;
+
 import com.alibaba.fastjson.JSONObject;
 import com.mzlion.easyokhttp.HttpClient;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
 
 @Component
 public class DingMsgUtil {
 
-//    @Value("${webSocket.msgUrl}")
-    private String msgUrl;
+	// @Value("${webSocket.msgUrl}")
+	private String msgUrl;
 
-    public void postMsg(String content) {
-        JSONObject msg = new JSONObject();
-        JSONObject text = new JSONObject();
-        msg.put("msgtype", "text");
-        text.put("content", content);
-        msg.put("text", text);
-        HttpClient.textBody(msgUrl)
-                // post提交json
-                .json(msg.toJSONString()).execute();
-    }
+	public void postMsg(String content) {
+		JSONObject msg = new JSONObject();
+		JSONObject text = new JSONObject();
+		msg.put("msgtype", "text");
+		text.put("content", content);
+		msg.put("text", text);
+		HttpClient.textBody(msgUrl)
+				// post提交json
+				.json(msg.toJSONString()).execute();
+	}
 }
