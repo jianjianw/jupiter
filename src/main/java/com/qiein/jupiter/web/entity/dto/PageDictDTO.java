9edfe633aa2079ad400a9fda3cd4791ac6265e5d
史涛 <st.email@qq.com>
2018-05-15 18:05:35 +0800

    base info  增加字典信息

--- src/main/java/com/qiein/jupiter/web/entity/dto/PageDictDTO.java
+++ src/main/java/com/qiein/jupiter/web/entity/dto/PageDictDTO.java
@@ -1,8 +1,10 @@
 package com.qiein.jupiter.web.entity.dto;
 
+import com.qiein.jupiter.web.entity.po.DictionaryPO;
 import com.qiein.jupiter.web.entity.po.StatusPO;
 import com.qiein.jupiter.web.entity.vo.SourceDictVO;
 
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -16,11 +18,22 @@ public class PageDictDTO {
      * 渠道字典
      */
     private Map<String, SourceDictVO> sourceMap;
-
     /**
      * 状态字典
      */
     private Map<String, StatusPO> statusMap;
+    /**
+     * 公共字典
+     */
+    private Map<String, List<DictionaryPO>> commonMap;
+
+    public Map<String, List<DictionaryPO>> getCommonMap() {
+        return commonMap;
+    }
+
+    public void setCommonMap(Map<String, List<DictionaryPO>> commonMap) {
+        this.commonMap = commonMap;
+    }
 
     public Map<String, SourceDictVO> getSourceMap() {
         return sourceMap;
