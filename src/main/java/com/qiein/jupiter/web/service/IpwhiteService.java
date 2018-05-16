3b626c5d8df7e6118ffe0a136e225c1050124030
项良 <871911653@qq.com>
2018-05-16 18:58:06 +0800

    ip白名单更新

--- src/main/java/com/qiein/jupiter/web/service/IpwhiteService.java
+++ src/main/java/com/qiein/jupiter/web/service/IpwhiteService.java
@@ -10,14 +10,34 @@ import com.qiein.jupiter.web.entity.po.IpWhitePO;
  **/
 public interface IpwhiteService {
 	
-	
+	/**
+	 * 新增
+	 *
+	 * @param ipWhitePo
+	 * @return
+	 */
 	public void insert(IpWhitePO ipWhitePo);
-	
+	/**
+	 * 删除
+	 *
+	 * @param id
+	 * @return
+	 */
 	public void delete(int id);
 	
-	
+	/**
+	 * 修改
+	 *
+	 * @param ipWhitePo
+	 * @return
+	 */
 	public void update(IpWhitePO ipWhitePo);
-
+	/**
+	 * 显示页面
+	 *
+	 * @param companyId
+	 * @return List<IpWhitePO>
+	 */
 	public List<IpWhitePO> get_all_ip_by_companyId(int companyId);
 	
 }
--- src/main/java/com/qiein/jupiter/web/service/impl/IpwhiteServiceImpl.java
+++ src/main/java/com/qiein/jupiter/web/service/impl/IpwhiteServiceImpl.java
@@ -19,25 +19,47 @@ public class IpwhiteServiceImpl implements IpwhiteService {
 
 	@Autowired
 	private IpwhiteDao ipwhitedao;
+	
+	/**
+	 * 新增
+	 *
+	 * @param ipWhitePo
+	 * @return
+	 */
 	@Override
 	public void insert(IpWhitePO ipWhitePo) {
 		// TODO Auto-generated method stub
 		ipwhitedao.insert(ipWhitePo);
 	}
-
+	/**
+	 * 删除
+	 *
+	 * @param id
+	 * @return
+	 */
 	@Override
 	public void delete(int id) {
 		// TODO Auto-generated method stub
 		ipwhitedao.delete(id);
 	}
 
-
+	/**
+	 * 修改
+	 *
+	 * @param ipWhitePo
+	 * @return
+	 */
 	@Override
 	public void update(IpWhitePO ipWhitePo) {
 		// TODO Auto-generated method stub
 		ipwhitedao.update(ipWhitePo);
 	}
-
+	/**
+	 * 显示页面
+	 *
+	 * @param companyId
+	 * @return List<IpWhitePO>
+	 */
 	@Override
 	public List<IpWhitePO> get_all_ip_by_companyId(int companyId) {
 		// TODO Auto-generated method stub
