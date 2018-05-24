35f30a8b1b76470da7e4a4eb394b4190c0f09fff
JingChenglong <chengvs_long@126.com>
2018-05-24 19:01:09 +0800

    1

--- src/main/java/com/qiein/jupiter/web/service/impl/ClientReceiveServiceImpl.java
+++ src/main/java/com/qiein/jupiter/web/service/impl/ClientReceiveServiceImpl.java
@@ -54,6 +54,12 @@ public class ClientReceiveServiceImpl implements ClientReceiveService {
 			// 一个客资领取
 			receive(kzId, Integer.valueOf(logId), companyId, staffId, staffName);
 		}
+
+		// 计算今日客资个数
+		resizeTodayNum(companyId, staffId);
+
+		// 推送页面重载客资列表
+		GoEasyUtil.pushInfoRefresh(companyId, staffId);
 	}
 
 	/**
@@ -61,7 +67,37 @@ public class ClientReceiveServiceImpl implements ClientReceiveService {
 	 */
 	@Override
 	public void refuse(String kzId, String logId, int companyId, int staffId, String staffName) {
+		if (StringUtil.haveEmpty(kzId, logId) || NumUtil.haveInvalid(companyId, staffId)) {
+			throw new RException(ExceptionEnum.INFO_ERROR);
+		}
+		if (kzId.length() > 32 && kzId.indexOf(CommonConstant.STR_SEPARATOR) != -1) {
+			// 多个客资拒接
+			String[] logIds = logId.split(CommonConstant.STR_SEPARATOR);
+			for (String id : logIds) {
+				refuse(kzId, Integer.valueOf(id), companyId, staffId, staffName);
+			}
+		} else {
+			// 一个客资拒接
+			refuse(kzId, Integer.valueOf(logId), companyId, staffId, staffName);
+		}
+	}
 
+	/**
+	 * 拒接客资
+	 * 
+	 * @param kzId
+	 * @param logId
+	 * @param companyId
+	 * @param staffId
+	 * @param staffName
+	 */
+	private void refuse(String kzId, int logId, int companyId, int staffId, String staffName) {
+		// 修改客资分配日志状态为已拒绝
+		int updateNum = clientAllotLogDao.updateAllogLog(DBSplitUtil.getAllotLogTabName(companyId), companyId, kzId,
+				logId, ClientConst.ALLOT_LOG_STATUS_REFUSE, "");
+		if (1 != updateNum) {
+			throw new RException(ExceptionEnum.ALLOT_LOG_ERROR);
+		}
 	}
 
 	/**
@@ -90,12 +126,6 @@ public class ClientReceiveServiceImpl implements ClientReceiveService {
 
 		// 修改客资信息
 		updateInfoWhenReceive(companyId, kzId, logId, staffId, staffName);
-
-		// 计算今日客资个数
-		resizeTodayNum(companyId, staffId);
-
-		// 推送页面重载客资列表
-		GoEasyUtil.pushInfoRefresh(companyId, staffId);
 	}
 
 	/**
@@ -163,6 +193,8 @@ public class ClientReceiveServiceImpl implements ClientReceiveService {
 	 * 领取多个客资
 	 */
 	private void receive(String[] kzIdArr, String[] logIdArr, int companyId, int staffId, String staffName) {
-
+		for (int i = 0; i < kzIdArr.length; i++) {
+			receive(kzIdArr[i], Integer.valueOf(logIdArr[i]), companyId, staffId, staffName);
+		}
 	}
 }
