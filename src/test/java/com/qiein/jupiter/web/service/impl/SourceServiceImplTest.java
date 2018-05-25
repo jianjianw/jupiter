06f8c173f552d306c34b253e06132d2f6b38482c
Tt <15267787927@163.com>
2018-05-25 17:18:01 +0800

    do some todo

---
+++ src/test/java/com/qiein/jupiter/web/service/impl/SourceServiceImplTest.java
@@ -0,0 +1,29 @@
+package com.qiein.jupiter.web.service.impl;
+
+import com.qiein.jupiter.web.dao.SourceDao;
+import com.qiein.jupiter.web.service.SourceService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Administrator on 2018/5/25 0025.
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SourceServiceImplTest {
+
+    @Autowired
+    private SourceDao sourceDao;
+
+    @Test
+    public void datDelSrc() throws Exception {
+        int i=sourceDao.datDelCheck("1,2,3".split(","),"hm_crm_client_info_1");
+        System.out.println(i);
+    }
+
+}
