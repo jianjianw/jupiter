353fc5a8085ce3c538a8ec94b03abd87168c9749
史涛 <st.email@qq.com>
2018-06-03 13:30:52 +0800

    定时清空 今日接单数目

--- src/main/java/com/qiein/jupiter/web/timer/TimerTask.java
+++ src/main/java/com/qiein/jupiter/web/timer/TimerTask.java
@@ -1,6 +1,7 @@
 package com.qiein.jupiter.web.timer;
 
 import com.qiein.jupiter.web.service.ClientPushService;
+import com.qiein.jupiter.web.service.StaffService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +9,8 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 /**
+ * 定时任务
+ *
  * @Author: shiTao
  */
 @Component
@@ -17,6 +20,9 @@ public class TimerTask {
     @Autowired
     private ClientPushService clientPushService;
 
+    @Autowired
+    private StaffService staffService;
+
     /**
      * 每天8-23点  2分钟执行一次
      */
@@ -24,5 +30,15 @@ public class TimerTask {
     public void pushClientNotice() {
         clientPushService.pushClientNoticeInfo();
         log.info("执行了定时推送提醒任务...");
+    }
+
+
+    /**
+     * 每天 0 点重置客服今天接单数目
+     */
+    @Scheduled(cron = "0 0 0 * * ?")
+    public void resetKfOderNum() {
+        staffService.taskClockInit();
+        log.info("定时清空了客服今天接单数目...");
     }
 }
