099ca4a2684f70d16628d4736e6a08c076576f21
JingChenglong <chengvs_long@126.com>
2018-06-03 13:29:19 +0800

    1

--- src/main/java/com/qiein/jupiter/web/entity/vo/PluginVO.java
+++ src/main/java/com/qiein/jupiter/web/entity/vo/PluginVO.java
@@ -4,17 +4,19 @@ import com.qiein.jupiter.web.entity.po.PluginPO;
 
 /**
  * 插件权限VO
+ * 
  * @create by Tt(叶华葳) 2018-06-02 11:15
  */
-public class PluginVO extends PluginPO{
-    //权限id
-    private Integer pmsId;
+public class PluginVO extends PluginPO {
+	private static final long serialVersionUID = 1L;
+	// 权限id
+	private Integer pmsId;
 
-    public Integer getPmsId() {
-        return pmsId;
-    }
+	public Integer getPmsId() {
+		return pmsId;
+	}
 
-    public void setPmsId(Integer pmsId) {
-        this.pmsId = pmsId;
-    }
+	public void setPmsId(Integer pmsId) {
+		this.pmsId = pmsId;
+	}
 }
