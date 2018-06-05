b14140d9bed40d56ed5c3615b081501e6a9b4581
Tt <15267787927@163.com>
2018-06-05 11:43:37 +0800

    1

--- src/main/java/com/qiein/jupiter/web/entity/vo/DictionaryVO.java
+++ src/main/java/com/qiein/jupiter/web/entity/vo/DictionaryVO.java
@@ -1,13 +1,22 @@
 package com.qiein.jupiter.web.entity.vo;
 
+import com.qiein.jupiter.web.entity.po.DictionaryPO;
+
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 /**
- * 新增咨询类型字典VO Created by Tt on 2018/5/23 0023.
+ * 新增咨询类型字典VO
+ * Created by Tt(叶华葳)
+ * on 2018/5/23 0023.
  */
 public class DictionaryVO implements Serializable {
 	private static final long serialVersionUID = 1L;
 	private String dicCodes;
+	private String dicNames;
+	private List<DictionaryPO> Diclist = new ArrayList<>();
 	private int companyId;
 
 	public int getCompanyId() {
@@ -24,5 +33,21 @@ public class DictionaryVO implements Serializable {
 
 	public void setDicCodes(String dicCodes) {
 		this.dicCodes = dicCodes;
+	}
+
+	public String getDicNames() {
+		return dicNames;
+	}
+
+	public void setDicNames(String dicNames) {
+		this.dicNames = dicNames;
+	}
+
+	public List<DictionaryPO> getDiclist() {
+		return Diclist;
+	}
+
+	public void setDiclist(List<DictionaryPO> diclist) {
+		Diclist = diclist;
 	}
 }
