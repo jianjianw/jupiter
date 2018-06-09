6dd1e1afc9a32daf086b886ec43f23aecf65e67e
Tt <15267787927@163.com>
2018-06-09 15:47:24 +0800

    1

--- src/main/java/com/qiein/jupiter/web/entity/vo/StaffDetailVO.java
+++ src/main/java/com/qiein/jupiter/web/entity/vo/StaffDetailVO.java
@@ -75,6 +75,19 @@ public class StaffDetailVO extends StaffPO {
      */
     private String msgSet;
 
+    /**
+     * 微信绑定标识 true绑定 false未绑定
+     */
+    private boolean weChatBindFlag;
+
+    public boolean isWeChatBindFlag() {
+        return weChatBindFlag;
+    }
+
+    public void setWeChatBindFlag(boolean weChatBindFlag) {
+        this.weChatBindFlag = weChatBindFlag;
+    }
+
     public String getMsgSet() {
         return msgSet;
     }
