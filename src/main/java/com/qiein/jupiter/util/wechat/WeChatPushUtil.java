979c3288916f66f1d31cda62a5d6c8566177abda
Tt <15267787927@163.com>
2018-06-10 11:08:39 +0800

    1

--- src/main/java/com/qiein/jupiter/util/wechat/WeChatPushUtil.java
+++ src/main/java/com/qiein/jupiter/util/wechat/WeChatPushUtil.java
@@ -31,8 +31,8 @@ public class WeChatPushUtil {
     //APPSECRET
     private final static String APP_SECRET = "c00e1dc5cf3c7c305ccf9e0b9dd6158e";    //"f1643abe4865080db153a0d181719005"
     //阿波罗地址
-//    private final static String APOLLO_URL ="http://uzymz6.natappfree.cc/";
-    private final static String APOLLO_URL ="http://127.0.0.1:80/";
+    private final static String APOLLO_URL ="http://uzymz6.natappfree.cc/";
+//    private final static String APOLLO_URL ="http://127.0.0.1:80/";
     //成功code
     private final static int SUCCESS_CODE = 100000;
 
