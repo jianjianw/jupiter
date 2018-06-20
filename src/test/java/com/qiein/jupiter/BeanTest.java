13ca2862de026ef459688f0f24b3157bac6decd7
yyx <rick.gson.yi@gmail.com>
2018-06-20 12:48:37 +0800

    1.金数据回调接口

---
+++ src/test/java/com/qiein/jupiter/BeanTest.java
@@ -0,0 +1,34 @@
+package com.qiein.jupiter;
+
+import com.qiein.jupiter.web.entity.po.GoldFingerPO;
+import org.junit.Test;
+
+/**
+ * FileName: BeanTest
+ *
+ * @author: yyx
+ * @Date: 2018-6-20 11:50
+ */
+public class BeanTest {
+    @Test
+    public void printJson(){
+        GoldFingerPO goldFingerPO = new GoldFingerPO();
+        goldFingerPO.setCompanyId(1);
+        goldFingerPO.setStaffId(2);
+        goldFingerPO.setAdId("123");
+        goldFingerPO.setAdAddress("https://www.baidu.com");
+        goldFingerPO.setCreateorName("小利");
+        goldFingerPO.setPostURL("http://114.55.249.156:9091/gold_data/add_client_info");
+        goldFingerPO.setIsFilter(0);
+        goldFingerPO.setIsShow(0);
+        goldFingerPO.setZxStyle("对话咨询");
+        goldFingerPO.setTypeId(2);
+        goldFingerPO.setTypeName("写真");
+        goldFingerPO.setSrcId(7);
+        goldFingerPO.setSrcName("支付宝口碑");
+        goldFingerPO.setFieldKey("field4,field5,filed6");
+        goldFingerPO.setFieldValue("kzqq,address,kzWw");
+        goldFingerPO.setFormId("34654");
+        goldFingerPO.setFormName("546");
+    }
+}
