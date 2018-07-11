8bb4f00cff62f00bcdc605a55f5a6146513de683
Tt <15267787927@163.com>
2018-07-11 11:10:58 +0800

    1

--- src/main/java/com/qiein/jupiter/web/controller/StaffController.java
+++ src/main/java/com/qiein/jupiter/web/controller/StaffController.java
@@ -296,13 +296,8 @@ public class StaffController extends BaseController {
 
         // 直接转发到平台
         String json = crmBaseApi.doService(map, "clientMoveLp");
-        System.out.println("asd: "+json);
-        JSONObject jsInfo = JsonFmtUtil.strInfoToJsonObj(json);
-//        if (!"100000".equals(jsInfo.getString("code")))
-//            throw new RException(jsInfo.getString("msg"));
-
         if (StringUtil.isEmpty(json) || !"100000".equalsIgnoreCase(JSONObject.parseObject(json).getJSONObject("response").getJSONObject("info").getString("code"))) {
-            return ResultInfoUtil.error(ExceptionEnum.UNKNOW_ERROR);
+            return ResultInfoUtil.error(ExceptionEnum.UNKNOW_ERROR.getCode(),JSONObject.parseObject(json).getJSONObject("response").getJSONObject("info").getString("msg"));
         }
         return ResultInfoUtil.success();
     }
