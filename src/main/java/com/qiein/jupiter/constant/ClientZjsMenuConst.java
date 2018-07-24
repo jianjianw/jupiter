52f5820cc8dd9b6cb35ad38e45af11767faa18d7
Tt <15267787927@163.com>
2018-07-24 14:53:32 +0800

    1

--- src/main/java/com/qiein/jupiter/constant/ClientZjsMenuConst.java
+++ src/main/java/com/qiein/jupiter/constant/ClientZjsMenuConst.java
@@ -9,33 +9,52 @@ import java.util.Map;
  * @Date: 2018/7/7 10:49
  */
 public class ClientZjsMenuConst {
-    public static Map<String,String> zjsMenu = new HashMap<>();
+    public static Map<String,String> QY_ZJS_MENU = new HashMap<>();
+    public static Map<String,String> LK_ZJS_MENU = new HashMap<>();
 
     static {
-        zjsMenu.put("kzname","客资姓名");
-        zjsMenu.put("kzphone","客资电话");
-        zjsMenu.put("kzwechat","客资微信");
-        zjsMenu.put("kzqq","客资qq");
-        zjsMenu.put("sex","客资性别");
-        zjsMenu.put("kzww","客资旺旺");
-        zjsMenu.put("matename","配偶姓名");
-        zjsMenu.put("matephone","配偶电话");
-        zjsMenu.put("matewechat","配偶微信");
-        zjsMenu.put("mateqq","配偶qq");
-        zjsMenu.put("shopname","门店名称");
-        zjsMenu.put("zxstyle","咨询方式");
-        zjsMenu.put("typeid","拍摄类型");
-        zjsMenu.put("address","客人地址");
-        zjsMenu.put("remark","备注");
-        zjsMenu.put("ysrange","预算范围");
-        zjsMenu.put("marrytime","婚期时间");
-        zjsMenu.put("yxlevel","意向等级");
-        zjsMenu.put("yptime","预拍时间");
-//        zjsMenu.put("channelname","所属渠道");
-//        zjsMenu.put("sourcename","所属来源");
-//        zjsMenu.put("srctype","来源类型");;
-////        zjsMenu.put("keyword","关键字");
-////        zjsMenu.put("adaddress","广告着陆页");
-////        zjsMenu.put("adid","广告ID");
+        QY_ZJS_MENU.put("kzname","客资姓名");
+        QY_ZJS_MENU.put("kzphone","客资电话");
+        QY_ZJS_MENU.put("kzwechat","客资微信");
+        QY_ZJS_MENU.put("kzqq","客资qq");
+        QY_ZJS_MENU.put("sex","客资性别");
+        QY_ZJS_MENU.put("kzww","客资旺旺");
+        QY_ZJS_MENU.put("matename","配偶姓名");
+        QY_ZJS_MENU.put("matephone","配偶电话");
+        QY_ZJS_MENU.put("matewechat","配偶微信");
+        QY_ZJS_MENU.put("mateqq","配偶qq");
+        QY_ZJS_MENU.put("shopname","门店名称");
+        QY_ZJS_MENU.put("zxstyle","咨询方式");
+        QY_ZJS_MENU.put("typeid","拍摄类型");
+        QY_ZJS_MENU.put("address","客人地址");
+        QY_ZJS_MENU.put("remark","备注");
+        QY_ZJS_MENU.put("ysrange","预算范围");
+        QY_ZJS_MENU.put("marrytime","婚期时间");
+        QY_ZJS_MENU.put("yxlevel","意向等级");
+        QY_ZJS_MENU.put("yptime","预拍时间");
+        //以上是亲友转介绍下拉菜单
+        //==========================
+        //以下是老客转介绍下拉菜单
+        LK_ZJS_MENU.put("kzname","客资姓名");
+        LK_ZJS_MENU.put("kzphone","客资电话");
+        LK_ZJS_MENU.put("kzwechat","客资微信");
+        LK_ZJS_MENU.put("kzqq","客资qq");
+        LK_ZJS_MENU.put("sex","客资性别");
+        LK_ZJS_MENU.put("kzww","客资旺旺");
+        LK_ZJS_MENU.put("matename","配偶姓名");
+        LK_ZJS_MENU.put("matephone","配偶电话");
+        LK_ZJS_MENU.put("matewechat","配偶微信");
+        LK_ZJS_MENU.put("mateqq","配偶qq");
+        LK_ZJS_MENU.put("shopname","门店名称");
+        LK_ZJS_MENU.put("zxstyle","咨询方式");
+        LK_ZJS_MENU.put("typeid","拍摄类型");
+        LK_ZJS_MENU.put("address","客人地址");
+        LK_ZJS_MENU.put("remark","备注");
+        LK_ZJS_MENU.put("ysrange","预算范围");
+        LK_ZJS_MENU.put("marrytime","婚期时间");
+        LK_ZJS_MENU.put("yxlevel","意向等级");
+        LK_ZJS_MENU.put("yptime","预拍时间");
+        LK_ZJS_MENU.put("oldkzname","老客姓名");
+        LK_ZJS_MENU.put("oldkzphone","老客电话");
     }
 }
