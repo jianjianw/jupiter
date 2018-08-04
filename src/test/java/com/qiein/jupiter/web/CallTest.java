3709429c41185aa0e5676f5dbe00128fb1727a10
yyx <rick.gson.yi@gmail.com>
2018-08-04 11:36:05 +0800

    1.小组新增迁移功能

--- src/test/java/com/qiein/jupiter/web/CallTest.java
+++ src/test/java/com/qiein/jupiter/web/CallTest.java
@@ -1,13 +1,10 @@
 package com.qiein.jupiter.web;
 
+import com.alibaba.fastjson.JSONObject;
 import com.aliyuncs.DefaultAcsClient;
 import com.aliyuncs.IAcsClient;
-import com.aliyuncs.auth.Credential;
-import com.aliyuncs.ccc.model.v20170705.CreateUserRequest;
-import com.aliyuncs.ecs.model.v20140526.DescribeInstancesRequest;
-import com.aliyuncs.ecs.model.v20140526.DescribeInstancesResponse;
-import com.aliyuncs.exceptions.ClientException;
-import com.aliyuncs.exceptions.ServerException;
+import com.aliyuncs.ccc.model.v20170705.StartBack2BackCallRequest;
+import com.aliyuncs.ccc.model.v20170705.StartBack2BackCallResponse;
 import com.aliyuncs.profile.DefaultProfile;
 
 /**
@@ -17,28 +14,46 @@ import com.aliyuncs.profile.DefaultProfile;
  * @Date: 2018-8-1 10:35
  */
 public class CallTest {
-    public static void main(String [] args){
+    public static void main(String [] args)throws  Exception{
         // 创建DefaultAcsClient实例并初始化
         DefaultProfile profile = DefaultProfile.getProfile(
-                "<your-region-id>",          // 地域ID
-                "<your-access-key-id>",      // RAM账号的AccessKey ID
-                "<your-access-key-secret>"); // RAM账号Access Key Secret
+                "cn-shanghai",          // 地域ID
+                "LTAIF0wvpMyT9Ood",      // RAM账号的AccessKey ID
+                "vpLFjXHUNcKUVud72qSBLXMZgD9AIo"); // RAM账号Access Key Secret
+
         IAcsClient client = new DefaultAcsClient(profile);
-        // 创建API请求并设置参数
-        CreateUserRequest createUserRequest = new CreateUserRequest();
-        DescribeInstancesRequest request = new DescribeInstancesRequest();
-        request.setPageSize(10);
-        // 发起请求并处理应答或异常
-        DescribeInstancesResponse response;
-        try {
-            response = client.getAcsResponse(request);
-            for (DescribeInstancesResponse.Instance instance:response.getInstances()) {
-                System.out.println(instance.getPublicIpAddress());
-            }
-        } catch (ServerException e) {
-            e.printStackTrace();
-        } catch (ClientException e) {
+
+
+        StartBack2BackCallRequest request1 = new StartBack2BackCallRequest();
+        request1.setCaller("15990187374");
+        request1.setCallee("13397117667");
+        request1.setInstanceId("d36fcfd0-bca5-46db-9efc-1947996f1cc8");
+        request1.setCallCenterNumber("057128284903");
+        StartBack2BackCallResponse response ;
+        try{
+            response = client.getAcsResponse(request1);
+            String taskId = response.getTaskId();
+            System.out.println(taskId);
+        }catch (Exception e){
             e.printStackTrace();
         }
+
     }
+
+
 }
+
+
+//        DescribeRegionsRequest request = new DescribeRegionsRequest();
+//        DescribeRegionsResponse response ;
+//        try {
+//            response = client.getAcsResponse(request);
+//            for (DescribeRegionsResponse.Region region:response.getRegions()) {
+//                System.out.println(JSONObject.toJSONString(region));
+//            }
+//        } catch (ServerException e) {
+//            e.printStackTrace();
+//        } catch (ClientException e) {
+//            e.printStackTrace();
+//        }
+
