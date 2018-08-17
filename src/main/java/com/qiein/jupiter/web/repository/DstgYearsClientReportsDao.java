0ed3e560e18b49f8aa8eb8fa7334bacaa405732b
yyx <rick.gson.yi@gmail.com>
2018-08-17 16:02:37 +0800

    1.年度报表增加合计

--- src/main/java/com/qiein/jupiter/web/repository/DstgYearsClientReportsDao.java
+++ src/main/java/com/qiein/jupiter/web/repository/DstgYearsClientReportsDao.java
@@ -2,9 +2,7 @@ package com.qiein.jupiter.web.repository;
 
 import com.qiein.jupiter.util.CollectionUtils;
 import com.qiein.jupiter.util.DBSplitUtil;
-import com.qiein.jupiter.web.entity.vo.DstgYearReportsVO;
-import com.qiein.jupiter.web.entity.vo.DsInvalidVO;
-import com.qiein.jupiter.web.entity.vo.ReportsParamVO;
+import com.qiein.jupiter.web.entity.vo.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
@@ -12,10 +10,7 @@ import org.springframework.stereotype.Repository;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
  * @author: yyx
@@ -25,19 +20,22 @@ import java.util.Map;
 public class DstgYearsClientReportsDao {
     @Autowired
     private JdbcTemplate jdbcTemplate;
+
     /**
      * 获取年度报表分析
-     * */
-    public List<DstgYearReportsVO> getDstgYearsClientReports(ReportsParamVO reportsParamVO, DsInvalidVO invalidConfig){
+     */
+    public List<DstgYearReportsVO> getDstgYearsClientReports(ReportsParamVO reportsParamVO, DsInvalidVO invalidConfig) {
         List<DstgYearReportsVO> dstgYearReportsVO = new ArrayList<>();
         //获取所有来源
-        getAllSource(reportsParamVO,dstgYearReportsVO);
+        getAllSource(reportsParamVO, dstgYearReportsVO);
         //获取所有年度客资数量
-        getAllYearClientCount(reportsParamVO,dstgYearReportsVO);
+        getAllYearClientCount(reportsParamVO, dstgYearReportsVO);
+        //computerTotal
+        computerTotal(reportsParamVO,dstgYearReportsVO);
         return dstgYearReportsVO;
     }
 
-    private void getAllSource(ReportsParamVO reportsParamVO,final List<DstgYearReportsVO> dstgYearReportsVO){
+    private void getAllSource(ReportsParamVO reportsParamVO, final List<DstgYearReportsVO> dstgYearReportsVO) {
         StringBuilder sb = new StringBuilder();
         sb.append(" select id,srcname,SRCIMG from hm_crm_source source where (source.TYPEID = 1 or source.typeid = 2)    and companyid = ?");
         List<DstgYearReportsVO> dstgYearReportsVOS = jdbcTemplate.query(sb.toString(), new Object[]{reportsParamVO.getCompanyId()}, new RowMapper<DstgYearReportsVO>() {
@@ -53,7 +51,7 @@ public class DstgYearsClientReportsDao {
         });
     }
 
-    private void getAllYearClientCount(ReportsParamVO reportsParamVO,final List<DstgYearReportsVO> dstgYearReportsVO){
+    private void getAllYearClientCount(ReportsParamVO reportsParamVO, final List<DstgYearReportsVO> dstgYearReportsVO) {
         StringBuilder sb = new StringBuilder();
         String infoTabName = DBSplitUtil.getInfoTabName(reportsParamVO.getCompanyId());
         sb.append(" SELECT t.myYear AS year,t.monthNo AS month,t.sourceid,sum(t.client_count) AS client_count  ");
@@ -61,31 +59,55 @@ public class DstgYearsClientReportsDao {
         sb.append(" SELECT MONTH(FROM_UNIXTIME(a.`CREATETIME`)) AS monthNo,  ");
         sb.append(" YEAR(FROM_UNIXTIME(a.`CREATETIME`)) AS myYear, ");
         sb.append("   count(a.id) AS client_count ,a.sourceid ");
-        sb.append(" FROM "+infoTabName +" a");
+        sb.append(" FROM " + infoTabName + " a");
         sb.append(" where a.companyid = ? and a.sourceid is not null");
         sb.append(" group by a.sourceid,monthNo) AS t ");
         sb.append(" WHERE t.myYear= ? ");
         sb.append(" GROUP BY t.monthNo,t.sourceid ");
         sb.append(" order by month ");
-        List<Map<String, Object>> mapList = jdbcTemplate.queryForList(sb.toString(), new Object[]{reportsParamVO.getCompanyId(),reportsParamVO.getYears()});
+        List<Map<String, Object>> mapList = jdbcTemplate.queryForList(sb.toString(), new Object[]{reportsParamVO.getCompanyId(), reportsParamVO.getYears()});
         //获取每月渠道客资数量
-        for (DstgYearReportsVO dstgYearReports:dstgYearReportsVO){
+        for (DstgYearReportsVO dstgYearReports : dstgYearReportsVO) {
             Map<String, Integer> newMap = null;
-            if(CollectionUtils.isNotEmpty(dstgYearReports.getMapList())){
+            if (CollectionUtils.isNotEmpty(dstgYearReports.getMapList())) {
                 newMap = dstgYearReports.getMapList();
-            }else{
-                newMap =  new HashMap();
+            } else {
+                newMap = new HashMap();
             }
-            for (Map map :mapList){
-                Integer sourceid =  Integer.parseInt(String.valueOf(map.get("sourceid").toString()));
-                String month = String.valueOf(map.get("month").toString()) ;
-                if(sourceid.equals(dstgYearReports.getSourceId())){
-                    newMap.put("month"+month,Integer.parseInt(String.valueOf(map.get("client_count").toString())) );
+            for (Map map : mapList) {
+                Integer sourceid = Integer.parseInt(String.valueOf(map.get("sourceid").toString()));
+                String month = String.valueOf(map.get("month").toString());
+                if (sourceid.equals(dstgYearReports.getSourceId())) {
+                    newMap.put("month" + month, Integer.parseInt(String.valueOf(map.get("client_count").toString())));
                 }
             }
             dstgYearReports.setMapList(newMap);
         }
 
     }
+
+    /**
+     * 计算总计
+     */
+    private void computerTotal(ReportsParamVO reportsParamVO, final List<DstgYearReportsVO> dstgYearReportsVOS) {
+        Map<String, Integer> kzNumMap = new HashMap();
+        DstgYearReportsVO dstgYearReportsVO = new DstgYearReportsVO();
+        dstgYearReportsVO.setSourceName("合计");
+        dstgYearReportsVO.setMapList(kzNumMap);
+
+        for (DstgYearReportsVO dstgYearReports : dstgYearReportsVOS) {
+            Map<String, Integer> mapList = dstgYearReports.getMapList();
+            for (Map.Entry<String, Integer> keys : mapList.entrySet()) {
+                Integer kzNum = kzNumMap.get(keys.getKey());
+                if (kzNum == null) {
+                    kzNum = 0;
+                }
+                kzNumMap.put(keys.getKey(), keys.getValue() + kzNum);
+            }
+        }
+        dstgYearReportsVO.setMapList(kzNumMap);
+        dstgYearReportsVOS.add(0,dstgYearReportsVO);
+    }
+
 
 }
