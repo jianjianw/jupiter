3d27bc7c340c6cb633e2e15f822477cf0df2792d
yyx <rick.gson.yi@gmail.com>
2018-08-31 16:13:06 +0800

    1

--- src/main/java/com/qiein/jupiter/web/repository/CommonReportsDao.java
+++ src/main/java/com/qiein/jupiter/web/repository/CommonReportsDao.java
@@ -1,5 +1,8 @@
 package com.qiein.jupiter.web.repository;
 
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+import com.qiein.jupiter.constant.ReportsConfigConst;
 import com.qiein.jupiter.util.NumUtil;
 import com.qiein.jupiter.web.entity.vo.DsInvalidVO;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,22 +31,69 @@ public class CommonReportsDao {
         if (NumUtil.isInValid(companyId)) {
             return null;
         }
-        sb.append(" SELECT comp.DSINVALIDSTATUS, comp.DSINVALIDLEVEL, comp.DDISVALID, comp.DSDDSTATUS ,comp.ZJSVALIDSTATUS FROM hm_pub_company comp WHERE comp.ID = ? AND comp.ISDEL = 0 ");
+        sb.append(" SELECT comp.REPORTSCONFIG  FROM hm_pub_company comp WHERE comp.ID = ? AND comp.ISDEL = 0 ");
+//        sb.append(" SELECT comp.DSINVALIDSTATUS, comp.DSINVALIDLEVEL, comp.DDISVALID, comp.DSDDSTATUS ,comp.ZJSVALIDSTATUS FROM hm_pub_company comp WHERE comp.ID = ? AND comp.ISDEL = 0 ");
         DsInvalidVO dsInvalidVO = jdbcTemplate.queryForObject(sb.toString(),
                 new Object[]{companyId},
                 new RowMapper<DsInvalidVO>() {
                     @Override
                     public DsInvalidVO mapRow(ResultSet rs, int i) throws SQLException {
                         DsInvalidVO dsInvalidVO = new DsInvalidVO();
-                        dsInvalidVO.setDsInvalidStatus(rs.getString("DSINVALIDSTATUS"));
-                        dsInvalidVO.setDsInvalidLevel(rs.getString("DSINVALIDLEVEL"));
-                        dsInvalidVO.setDdIsValid(rs.getBoolean("DDISVALID"));
-                        dsInvalidVO.setDsDdStatus(rs.getString("DSDDSTATUS"));
-                        dsInvalidVO.setZjsValidStatus(rs.getString("ZJSVALIDSTATUS"));
+                        JSONObject configObj = JSONObject.parseObject(rs.getString("REPORTSCONFIG"));
+                        //筛选无效的意向等级
+                        JSONArray yxLevelArr = configObj.getJSONObject(ReportsConfigConst.WX_SET).getJSONArray(ReportsConfigConst.DZZ_YXDJ);
+                        //筛选无效状态
+                        JSONArray statusArr = configObj.getJSONObject(ReportsConfigConst.WX_SET).getJSONArray(ReportsConfigConst.WX_STATUS);
+                        //待定状态
+                        JSONArray xkzArr = configObj.getJSONObject(ReportsConfigConst.DS_DD_SET).getJSONArray(ReportsConfigConst.XKZ_CLASS);
+                        JSONArray yyyArr = configObj.getJSONObject(ReportsConfigConst.DS_DD_SET).getJSONArray(ReportsConfigConst.YYY_CLASS);
+                        JSONArray dzzArr = configObj.getJSONObject(ReportsConfigConst.DS_DD_SET).getJSONArray(ReportsConfigConst.DZZ_CLASS);
+                        JSONArray yjdArr = configObj.getJSONObject(ReportsConfigConst.DS_DD_SET).getJSONArray(ReportsConfigConst.YJD_CLASS);
+                        JSONArray yddArr = configObj.getJSONObject(ReportsConfigConst.DS_DD_SET).getJSONArray(ReportsConfigConst.YDD_CLASS);
+                        JSONArray wxArr = configObj.getJSONObject(ReportsConfigConst.DS_DD_SET).getJSONArray(ReportsConfigConst.WX_CLASS);
+                        JSONArray dsDdStatusArr = new JSONArray();
+                        dsDdStatusArr.addAll(xkzArr);
+                        dsDdStatusArr.addAll(yyyArr);
+                        dsDdStatusArr.addAll(dzzArr);
+                        dsDdStatusArr.addAll(yjdArr);
+                        dsDdStatusArr.addAll(yddArr);
+                        dsDdStatusArr.addAll(wxArr);
+                        //转介绍状态
+                        JSONArray zjsXkzArr = configObj.getJSONObject(ReportsConfigConst.ZJS_VALID_SET).getJSONArray(ReportsConfigConst.XKZ_CLASS);
+                        JSONArray zjsYyyArr = configObj.getJSONObject(ReportsConfigConst.ZJS_VALID_SET).getJSONArray(ReportsConfigConst.YYY_CLASS);
+                        JSONArray zjsDzzArr = configObj.getJSONObject(ReportsConfigConst.ZJS_VALID_SET).getJSONArray(ReportsConfigConst.DZZ_CLASS);
+                        JSONArray zjsYjdArr = configObj.getJSONObject(ReportsConfigConst.ZJS_VALID_SET).getJSONArray(ReportsConfigConst.YJD_CLASS);
+                        JSONArray zjsYddArr = configObj.getJSONObject(ReportsConfigConst.ZJS_VALID_SET).getJSONArray(ReportsConfigConst.YDD_CLASS);
+                        JSONArray zjsWxArr = configObj.getJSONObject(ReportsConfigConst.ZJS_VALID_SET).getJSONArray(ReportsConfigConst.WX_CLASS);
+                        JSONArray zjsDdStatusArr = new JSONArray();
+                        zjsDdStatusArr.addAll(zjsXkzArr);
+                        zjsDdStatusArr.addAll(zjsYyyArr);
+                        zjsDdStatusArr.addAll(zjsDzzArr);
+                        zjsDdStatusArr.addAll(zjsYjdArr);
+                        zjsDdStatusArr.addAll(zjsYddArr);
+                        zjsDdStatusArr.addAll(zjsWxArr);
+
+                        dsInvalidVO.setDsInvalidStatus(arrToStr(yxLevelArr));
+                        dsInvalidVO.setDsInvalidLevel(arrToStr(statusArr));
+                        dsInvalidVO.setDdIsValid(configObj.getJSONObject(ReportsConfigConst.YX_SET).getBoolean(ReportsConfigConst.DD_IS_YX));
+                        dsInvalidVO.setDsDdStatus(arrToStr(dsDdStatusArr));
+                        dsInvalidVO.setZjsValidStatus(arrToStr(zjsDdStatusArr));
+
                         return dsInvalidVO;
                     }
                 });
         return dsInvalidVO;
+    }
+
+    /**
+     * 数组转字符串
+     * */
+    private String arrToStr(JSONArray jsonArray){
+        String str = "";
+        if(!jsonArray.isEmpty()){
+            str = jsonArray.toString().substring(1, jsonArray.toString().length() - 1);
+        }
+        return str;
     }
 
 
