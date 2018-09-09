1bb760eb4c34e75bc773f281ab1bde499c5f6d57
史涛 <st.email@qq.com>
2018-09-09 11:44:02 +0800

    显示typeid

--- src/main/java/com/qiein/jupiter/web/entity/vo/ChannelVO.java
+++ src/main/java/com/qiein/jupiter/web/entity/vo/ChannelVO.java
@@ -7,41 +7,58 @@ import java.util.LinkedList;
  * 渠道来源集合
  */
 public class ChannelVO implements Serializable {
-	private static final long serialVersionUID = 1L;
-	/**
-	 * 渠道ID
-	 */
-	private int channelId;
-	/**
-	 * 渠道名称
-	 */
-	private String channelName;
-	/**
-	 * 来源集合
-	 */
-	private LinkedList<SrcListVO> srcList;
+    private static final long serialVersionUID = 1L;
+    /**
+     * 渠道ID
+     */
+    private int channelId;
+    /**
+     * 渠道名称
+     */
+    private String channelName;
+    /**
+     * 渠道类型
+     */
+    private int typeId;
+    /**
+     * 来源集合
+     */
+    private LinkedList<SrcListVO> srcList;
 
-	public int getChannelId() {
-		return channelId;
-	}
 
-	public void setChannelId(int channelId) {
-		this.channelId = channelId;
-	}
+    public static long getSerialVersionUID() {
+        return serialVersionUID;
+    }
 
-	public String getChannelName() {
-		return channelName;
-	}
+    public int getTypeId() {
+        return typeId;
+    }
 
-	public void setChannelName(String channelName) {
-		this.channelName = channelName;
-	}
+    public void setTypeId(int typeId) {
+        this.typeId = typeId;
+    }
 
-	public LinkedList<SrcListVO> getSrcList() {
-		return srcList;
-	}
+    public int getChannelId() {
+        return channelId;
+    }
 
-	public void setSrcList(LinkedList<SrcListVO> srcList) {
-		this.srcList = srcList;
-	}
+    public void setChannelId(int channelId) {
+        this.channelId = channelId;
+    }
+
+    public String getChannelName() {
+        return channelName;
+    }
+
+    public void setChannelName(String channelName) {
+        this.channelName = channelName;
+    }
+
+    public LinkedList<SrcListVO> getSrcList() {
+        return srcList;
+    }
+
+    public void setSrcList(LinkedList<SrcListVO> srcList) {
+        this.srcList = srcList;
+    }
 }
