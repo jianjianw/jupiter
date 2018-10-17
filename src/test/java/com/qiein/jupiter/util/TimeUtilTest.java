77392bcfc40cb6048e9368bb1aef2febe439828a
史涛 <st.email@qq.com>
2018-10-17 15:11:07 +0800

    时间BUG

--- src/test/java/com/qiein/jupiter/util/TimeUtilTest.java
+++ src/test/java/com/qiein/jupiter/util/TimeUtilTest.java
@@ -1,6 +1,7 @@
 package com.qiein.jupiter.util;
 
 import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 import org.junit.Test;
 
 import java.util.Calendar;
@@ -38,16 +39,24 @@ public class TimeUtilTest {
         Calendar calendar1 = Calendar.getInstance();
         //设置昨天
         calendar1.setTime(new Date());
-        calendar1.set(Calendar.HOUR, 0);
+        calendar1.set(Calendar.HOUR_OF_DAY, 0);
         calendar1.set(Calendar.MINUTE, 0);
         calendar1.set(Calendar.SECOND, 0);
+        System.out.println(calendar1.getTime());
         Calendar calendar2 = Calendar.getInstance();
         calendar2.setTime(new Date());
         calendar2.set(Calendar.HOUR_OF_DAY, 23);
         calendar2.set(Calendar.MINUTE, 59);
         calendar2.set(Calendar.SECOND, 59);
+        System.out.println(calendar2.getTime());
         map.put("start", TimeUtil.getTimeStamp10ByDate(calendar1.getTime()));
         map.put("end", TimeUtil.getTimeStamp10ByDate(calendar2.getTime()));
         System.out.println(JSON.toJSONString(map));
+    }
+
+    @Test
+    public void test123() {
+        Map<String, Integer> todayTimeInterval = TimeUtil.getTodayTimeInterval();
+        System.out.println(JSONObject.toJSONString(todayTimeInterval));
     }
 }
