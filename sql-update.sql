12d1dced1ca3a8473068db7464e5c81464f98146
sheery <727095262@qq.com>
2018-10-19 11:29:17 +0800

    1

--- sql-update.sql
+++ sql-update.sql
@@ -8,7 +8,10 @@ COMMENT '报表配置'
 ###9.14
 client info 表新增 LETTERID  短字母编号
 
-
+###  10/15   添加收款类型字段
+ALTER TABLE hm_crm_cash_log
+  ADD `TYPEID` smallint(6) DEFAULT 1
+COMMENT '收款类型  1：首款  2：补款'
 
 ###  10/19
 增加   报表设置  、录入指定客服权限
