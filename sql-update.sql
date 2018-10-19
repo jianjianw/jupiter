b6137a5a838c772a8071bd0e18db28ac92baf5a0
史涛 <st.email@qq.com>
2018-10-19 15:38:51 +0800

    客资重复查询

--- sql-update.sql
+++ sql-update.sql
@@ -7,3 +7,16 @@ COMMENT '报表配置'
 
 ###9.14
 client info 表新增 LETTERID  短字母编号
+
+
+###10月
+员工表staff
+新增
+WHEELFLAG	tinyint	255	0	-1	0	0	0			0	轮单标志					0	0	0	0	0	0	0
+SETTINGS	varchar	1024	0	-1	0	0	0				个人设置			utf8	utf8_general_ci	0	0	0	0	0	0	0
+
+page_conf
+ACTIONSQL	varchar	1024	0	-1	0	0	0				action的SQL语句			utf8	utf8_general_ci	0	0	0	0	0	0	0
+SQLCONDITION	varchar	2048	0	-1	0	0	0				SQL条件的序列化			utf8	utf8_general_ci	0	0	0	0	0	0	0
+SHOWFLAG	bit	1	0	-1	0	0	0			b'1'	显示标志					0	0	0	0	0	0	0
+
