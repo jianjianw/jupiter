034391d1e6d703f20f3addce216f29bb15c0b046
sheery <727095262@qq.com>
2018-10-23 10:28:08 +0800

    1

--- sql-update.sql
+++ sql-update.sql
@@ -29,3 +29,32 @@ COMMENT '收款类型  1：首款  2：补款'
 INSERT INTO  hm_pub_permission (`ID`, `PERMISSIONNAME`, `TYPEID`) VALUES (202, '报表设置', 1);
 INSERT INTO  hm_pub_permission (`ID`, `PERMISSIONNAME`, `TYPEID`) VALUES (203, '录入指定客服', 1);
 INSERT INTO  hm_pub_permission (`ID`, `PERMISSIONNAME`, `TYPEID`) VALUES (204, '修改客资联系方式', 1);
+
+
+###  10/23   增加报表权限
+INSERT INTO hm_pub_permission(`ID`, `PERMISSIONNAME`, `TYPEID`) VALUES (205, '电商推广-来源统计', 1);
+INSERT INTO hm_pub_permission(`ID`, `PERMISSIONNAME`, `TYPEID`) VALUES (206, '电商推广-渠道统计', 1);
+INSERT INTO hm_pub_permission(`ID`, `PERMISSIONNAME`, `TYPEID`) VALUES (207, '电商推广-推广统计', 1);
+INSERT INTO hm_pub_permission(`ID`, `PERMISSIONNAME`, `TYPEID`) VALUES (208, '电商推广-客资状态', 1);
+INSERT INTO hm_pub_permission(`ID`, `PERMISSIONNAME`, `TYPEID`) VALUES (209, '电商推广-月度统计', 1);
+INSERT INTO hm_pub_permission(`ID`, `PERMISSIONNAME`, `TYPEID`) VALUES (210, '电商推广-年度统计', 1);
+INSERT INTO hm_pub_permission(`ID`, `PERMISSIONNAME`, `TYPEID`) VALUES (211, '电商推广-广告信息', 1);
+INSERT INTO hm_pub_permission(`ID`, `PERMISSIONNAME`, `TYPEID`) VALUES (212, '电商推广-咨询方式', 1);
+INSERT INTO hm_pub_permission(`ID`, `PERMISSIONNAME`, `TYPEID`) VALUES (213, '电商推广-省域分析', 1);
+INSERT INTO hm_pub_permission(`ID`, `PERMISSIONNAME`, `TYPEID`) VALUES (214, '电商推广-市域分析', 1);
+INSERT INTO hm_pub_permission(`ID`, `PERMISSIONNAME`, `TYPEID`) VALUES (215, '电商推广-关键词分析', 1);
+INSERT INTO hm_pub_permission(`ID`, `PERMISSIONNAME`, `TYPEID`) VALUES (216, '电商推广-无效原因', 1);
+INSERT INTO hm_pub_permission(`ID`, `PERMISSIONNAME`, `TYPEID`) VALUES (217, '电商客服-客服数据', 1);
+INSERT INTO hm_pub_permission(`ID`, `PERMISSIONNAME`, `TYPEID`) VALUES (218, '电商客服-无效原因', 1);
+INSERT INTO hm_pub_permission(`ID`, `PERMISSIONNAME`, `TYPEID`) VALUES (219, '电商客服-客资状态', 1);
+INSERT INTO hm_pub_permission(`ID`, `PERMISSIONNAME`, `TYPEID`) VALUES (220, '转介绍-来源分析', 1);
+INSERT INTO hm_pub_permission(`ID`, `PERMISSIONNAME`, `TYPEID`) VALUES (221, '转介绍-邀约统计', 1);
+INSERT INTO hm_pub_permission(`ID`, `PERMISSIONNAME`, `TYPEID`) VALUES (222, '转介绍-月度统计', 1);
+INSERT INTO hm_pub_permission(`ID`, `PERMISSIONNAME`, `TYPEID`) VALUES (223, '转介绍-年度统计', 1);
+INSERT INTO hm_pub_permission(`ID`, `PERMISSIONNAME`, `TYPEID`) VALUES (224, '转介绍-全员提报', 1);
+INSERT INTO hm_pub_permission(`ID`, `PERMISSIONNAME`, `TYPEID`) VALUES (225, '转介绍-老客统计', 1);
+INSERT INTO hm_pub_permission(`ID`, `PERMISSIONNAME`, `TYPEID`) VALUES (226, '门市-入店统计', 1);
+INSERT INTO hm_pub_permission(`ID`, `PERMISSIONNAME`, `TYPEID`) VALUES (227, '财务-渠道订单统计', 1);
+INSERT INTO hm_pub_permission(`ID`, `PERMISSIONNAME`, `TYPEID`) VALUES (228, '财务-月度订单统计', 1);
+INSERT INTO hm_pub_permission(`ID`, `PERMISSIONNAME`, `TYPEID`) VALUES (229, '其他-联系方式修改', 1);
+INSERT INTO hm_pub_permission(`ID`, `PERMISSIONNAME`, `TYPEID`) VALUES (230, '其他-微信扫码记录', 1);
