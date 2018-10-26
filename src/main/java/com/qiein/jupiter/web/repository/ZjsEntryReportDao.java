1ed30d840ec458682bb54e37a1eab345832669c4
wanglei <13992787306@163.com>
2018-10-26 15:48:36 +0800

    迁移报表

--- src/main/java/com/qiein/jupiter/web/repository/ZjsEntryReportDao.java
+++ src/main/java/com/qiein/jupiter/web/repository/ZjsEntryReportDao.java
@@ -14,6 +14,7 @@ import com.qiein.jupiter.web.entity.vo.ZjsSourceVO;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
 
 
 import javax.sql.RowSet;
@@ -26,6 +27,7 @@ import java.util.Map;
 /**
  * 转介绍提报统计
  */
+@Repository
 public class ZjsEntryReportDao {
 
 
